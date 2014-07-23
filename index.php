f5547de888a1b15aea8e213191091dc2c015703f
Vyas <vyas.vk1989@gmail.com>
2014-07-22 19:43:08 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <html>
+<head>
+<link href="index.css" type="text/css" rel="stylesheet" />
+<title>
+Dropbox History Browser</title></head>
 <body>
 <?php
-print "<p>Hello world</p>";
-?>
+print "<p>Hello world</p>"; ?>
 </body>
 </html>
